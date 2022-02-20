5d31555df93a750a9642f604c6813c32c5fce92e
Glauciagmm <glauciamesquitagmm@hotmail.com>
2022-02-20 23:21:36 +0100

    archivos html css media javascript

---
+++ script.js
@@ -0,0 +1,3 @@
+var botonpiruleta;
+botonpiruleta = document.getElementById('agotado');
+botonpiruleta.addEventListener('click', function(){alert("Este producto se encuentra agotado temporalmente");});
